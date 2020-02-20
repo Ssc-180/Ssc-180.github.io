46390ebf023dabf373d53c7e3bdda864a13ab462
Ssc-180 <976535785@qq.com>
2020-02-20 17:51:43 +0800

    Site updated: 2020-02-20 17:51:42

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+ container:
+document.getElementById('aplayer'),
+ fixed: true,
+ autoplay: false,
+ audio: [
+ {
+ name: "【say it again】",
+ artist: '【盘尼西林】',
+ url: '【http://isure.stream.qqmusic.qq.com/C100002BJDiQ0ZgYTA.m4a?fromtag=32】',
+ cover: '【第⼀⾸歌歌曲封⾯】',
+ }
+ ]
+});
