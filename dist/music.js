f5bb93fc01066d213d385bab8e5935b9ee1e4876
Ssc-180 <976535785@qq.com>
2020-02-20 18:28:59 +0800

    Site updated: 2020-02-20 18:28:58

--- dist/music.js
+++ dist/music.js
@@ -6,9 +6,7 @@ const ap = new APlayer({
       {
         name: "Say It Again",
         artist: '盘尼西林',
-        url: 'https://sharefs.yun.kugou.com/202002201758/373d57f33f286c1f5452d7219d7e877f/G098/M06/13/05/og0DAFj4uy-AfuAkAFKo0B12RiE069.mp3 
-
-',
+        url: 'https://sharefs.yun.kugou.com/202002201758/373d57f33f286c1f5452d7219d7e877f/G098/M06/13/05/og0DAFj4uy-AfuAkAFKo0B12RiE069.mp3',
         cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190924/20190924120603913030.jpg',
       }
     ]
