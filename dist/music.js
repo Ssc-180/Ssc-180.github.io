c5b75f415afed0ad6c27db72c7451016781d0c9c
Ssc-180 <976535785@qq.com>
2020-02-20 18:02:37 +0800

    Site updated: 2020-02-20 18:02:36

--- dist/music.js
+++ dist/music.js
@@ -7,8 +7,10 @@ document.getElementById('aplayer'),
  {
  name: "say it again",
  artist: '盘尼西林',
- url: 'http://isure.stream.qqmusic.qq.com/C100002BJDiQ0ZgYTA.m4a?fromtag=32',
- cover: '第⼀⾸歌歌曲封⾯',
+ url: 'https://sharefs.yun.kugou.com/202002201758/373d57f33f286c1f5452d7219d7e877f/G098/M06/13/05/og0DAFj4uy-AfuAkAFKo0B12RiE069.mp3 
+',
+ cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190924/20190924120603913030.jpg
+',
  }
  ]
 });
