0d57c19e94779cfb0ce07211ef0a3d4abde76cd4
Ssc-180 <976535785@qq.com>
2020-02-23 20:10:00 +0800

    Site updated: 2020-02-23 20:10:00

--- dist/music.js
+++ dist/music.js
@@ -9,7 +9,6 @@ const ap = new APlayer({
       artist: '盘尼西林',
       url: 'https://gitee.com/ZPSource/BinRepo/raw/master/%E7%9B%98%E5%B0%BC%E8%A5%BF%E6%9E%97%20-%20Say%20It%20Again.mp3',
       cover: 'https://gitee.com/ZPSource/BinRepo/raw/master/%E7%9B%98%E5%B0%BC%E8%A5%BF%E6%9E%97%20-%20Say%20It%20Again%20-%20Cover.jpg',
-      lrc: 'https://gitee.com/ZPSource/BinRepo/raw/master/%E7%9B%98%E5%B0%BC%E8%A5%BF%E6%9E%97%20-%20Say%20It%20Again%20-%20Lyrics.lrc',
     }
   ]
 });
